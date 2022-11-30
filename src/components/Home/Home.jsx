0c8823478e0edf2f09a67e7c79b89e1932b12eab
Jadson-19 <98785746+Jadson-19@users.noreply.github.com>
2022-11-30 01:54:27 -0300

    Add files via upload

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function Home() {
+  return (
+    <section>
+      <div class="pagInicial">
+        <h2>
+          {" "}
+          Com um controle de finanças moderno e atualizado, o usuário garantirá
+          uma previsibilidade financeira poderosa cadastrando suas despesas em
+          tempo real, de onde estiver. Através da plataforma, será possível
+          lançar os gastos do dia a dia e acompanhar os relatórios sempre que
+          possível. Assuma já o controle da sua vida financeira.{" "}
+        </h2>{" "}
+        <br></br>
+        <h2>Vamos começar?</h2>
+        <Link to="/SubHome">
+          <button>
+            Gerenciar Finanças <i className="fa fa-arrow-right"></i>
+          </button>
+        </Link>
+      </div>
+     
+    </section>
+
+
+  );
+  
+}
+
+export default Home;
